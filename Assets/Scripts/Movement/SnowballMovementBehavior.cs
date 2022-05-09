bc6131cd2534ffad8ea30df2b2ae2c518d37219f
Michael Lofton <michaellofton32@gmail.com>
2022-05-08 20:19:17 -0500

    Implemented snowball movement and invisible barriers

--- Assets/Scripts/Movement/SnowballMovementBehavior.cs
+++ Assets/Scripts/Movement/SnowballMovementBehavior.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class SnowballMovementBehavior : MonoBehaviour
 {
-    private Vector2 _velocity;
+    [SerializeField]
+    private float _speed;
+    private Vector3 _velocity;
+    [SerializeField]
+    private Vector3 _moveDirection = Vector3.left;
+    private bool _isGrounded;
 
-    private Vector2 Velocity
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public Vector3 Velocity
     {
         get { return _velocity; }
         set { _velocity = value; }
@@ -14,6 +25,32 @@ public class SnowballMovementBehavior : MonoBehaviour
 
     private void Update()
     {
-        
+        if (_isGrounded)
+        {
+            //Vector3 direction = Vector3.left;
+            Velocity = _moveDirection.normalized * Speed;
+            transform.position += Velocity * Time.deltaTime;
+        } 
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _isGrounded = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Barrier")
+        {
+            if (_moveDirection == Vector3.left)
+                _moveDirection = Vector3.right;
+            else if (_moveDirection == Vector3.right)
+                _moveDirection = Vector3.left;
+        }
     }
 }
