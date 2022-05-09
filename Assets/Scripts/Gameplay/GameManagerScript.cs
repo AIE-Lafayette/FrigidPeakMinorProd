a8eb3d27d7e9e65bbd5e32ad44d220810e3e532f
Will <montero_w@live.com>
2022-05-09 10:47:56 -0500

    Updating Current Changes
    Created Upgrade Spawn Script Updated Current Game Manager Script. (Added Comments)

--- Assets/Scripts/Gameplay/GameManagerScript.cs
+++ Assets/Scripts/Gameplay/GameManagerScript.cs
@@ -7,20 +7,21 @@ public class GameManagerScript : MonoBehaviour
     private Text _gameTimer;
 
     static private int _currentLives = 3;
-    static private int _clectables = 0;
+    static private int _collectables = 0;
     static private float _currentGameTimer = 0;
     static private float _gameScore = 0;
 
     //Tracks Player Current Lives 
-    static public int CurrentLives { get { return _currentLives; } set { _currentLives = value; } }
-    //Trackscollectable collected 
-    static public int Celectables { get { return _clectables; } set { _clectables = value; } }
+    static public int CurrentLives { get { return _currentLives; } }
+    //Tracks collectable collected 
+    static public int Collectables { get { return _collectables; } }
+
     static public float CurrentGameTimer { get { return _currentGameTimer; } }
-    static public float GameScore { get { return _gameScore; } set { _gameScore = value; } }
+    static public float GameScore { get { return _gameScore; } }
 
     private void Start()
     {
-        _currentGameTimer += 1 + Time.deltaTime; 
+
     }
 
     private void Update()
@@ -30,12 +31,31 @@ public class GameManagerScript : MonoBehaviour
 
 
     private void TimeClock()
-    {    
-        float milaSeconds = Mathf.FloorToInt(_currentGameTimer % 60);
-        float seconds = Mathf.FloorToInt(_currentGameTimer / 60);
-        float min = Mathf.FloorToInt(seconds / 60);
+    {
+        _currentGameTimer += 1 + Time.deltaTime;
+        float milaSeconds = Mathf.FloorToInt(_currentGameTimer / 60);
+        float seconds = Mathf.FloorToInt(milaSeconds % 60);
+        float min = Mathf.FloorToInt(milaSeconds / 60);
         string timer = string.Format("{0:00}:{1:00}" , min, seconds);
         _gameTimer.text = timer;
+    }
+
+    //Adds a point to the callectagble
+    static public void CollectedACollectable()
+    {
+        _collectables++;
+    }
+
+    //Increase the point based on the amount of pints earned 
+    static public void IncreaseScore(float points = 1)
+    {
+        _gameScore += points;
+    }
+
+    //Removes a life from current lives 
+    static public void LostALife()
+    {
+        _currentLives--;
     }
 }
 
