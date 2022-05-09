847db95f881a6624d5a8931583e8979e6740e949
mikeLofton <michaellofton32@gmail.com>
2022-05-09 11:24:09 -0500

    Comments

--- Assets/Scripts/Gameplay/SnowballDespawnBehavior.cs
+++ Assets/Scripts/Gameplay/SnowballDespawnBehavior.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SnowballDespawnBehavior : MonoBehaviour
 {
-    private float _currentLife;
+    private float _currentLife; //How long the snowball has existed 
     [SerializeField]
-    private float _lifetime = 10.0f;
+    private float _lifetime = 10.0f; //How lonf the snowball can exist
 
     private void Update()
     {
         _currentLife += Time.deltaTime;
 
-        if (_currentLife >= _lifetime)
+        if (_currentLife >= _lifetime) //If the current lif is greater than its lifetime destroy object
             Destroy(gameObject);
     }
 }
--- Assets/Scripts/Gameplay/SnowballSpawnBehavior.cs
+++ Assets/Scripts/Gameplay/SnowballSpawnBehavior.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class SnowballSpawnBehavior : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _snowball;
+    private GameObject _snowball; //The snowball to clone and spawn
     [SerializeField]
-    private float _cooldown;
+    private float _cooldown; //Cooldown between spawns
     private float _spawnTime = 0.0f;
 
     private void Update()
     {
-        if (_spawnTime >= _cooldown)
+        if (_spawnTime >= _cooldown) //If spawn time is greater than the cooldown
         {
-            GameObject spawnedObject = Instantiate(_snowball, transform.position, transform.rotation);
+            GameObject spawnedObject = Instantiate(_snowball, transform.position, transform.rotation); //Instantiate the snowballs 
             _spawnTime = 0.0f;
         }
         _spawnTime += Time.deltaTime;
