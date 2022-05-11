90a4d5841ea39f469105d7df2ca7356b5a16601c
mikeLofton <michaellofton32@gmail.com>
2022-05-11 16:13:46 -0500

    Win manager comments

--- Assets/Scripts/Gameplay/WinManagerScript.cs
+++ Assets/Scripts/Gameplay/WinManagerScript.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class WinManagerScript : MonoBehaviour
 {
     [SerializeField]
-    private Text _winText;
+    private Text _winText; //The win Text
 
     private void OnCollisionEnter(Collision collision)
     {
+        //On collision with the player display the win text
         if (collision.gameObject.tag == "player")
         {
             _winText.text = "YOU WIN";
