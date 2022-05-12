3ee5aac91994dd0319b56c3d7eb9df47064d82cb
KatelynWest <katelynwest225@gmail.com>
2022-05-12 17:03:14 -0500

    Fixed Rope Behavior

--- Assets/Scripts/Gameplay/RopeBehavior.cs
+++ Assets/Scripts/Gameplay/RopeBehavior.cs
@@ -14,6 +14,7 @@ public class RopeBehavior : MonoBehaviour
             _playerMovement = other.GetComponent<PlayerMovementBehavior>();
             _playerMovement.CanGoVertical = true;
             _playerMovement.IsOnRope = true;
+            _playerMovement.GetComponent<Rigidbody>().useGravity = false;
         }
     }
 
@@ -24,6 +25,7 @@ public class RopeBehavior : MonoBehaviour
             _playerMovement = other.GetComponent<PlayerMovementBehavior>();
             _playerMovement.CanGoVertical = false;
             _playerMovement.IsOnRope = false;
+            _playerMovement.GetComponent<Rigidbody>().useGravity = true;
         }
     }
 }
