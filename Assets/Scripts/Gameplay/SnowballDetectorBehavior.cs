92a1145154c077964083a02187ab6721458e83e7
Katelyn <katelynwest225@gmail.com>
2022-05-25 13:04:09 -0500

    Made the Snowball detector a collider rather than a raycast

--- Assets/Scripts/Gameplay/SnowballDetectorBehavior.cs
+++ Assets/Scripts/Gameplay/SnowballDetectorBehavior.cs
@@ -4,31 +4,19 @@ using UnityEngine;
 
 public class SnowballDetectorBehavior : MonoBehaviour
 {
-    private Vector3 _rayDir;
-    private Ray _detector;
+    PlayerMovementBehavior _playerMovement;
 
     private void Awake()
     {
-        _rayDir = -transform.up * 100;
-        _detector = new Ray(transform.position, _rayDir);
+        _playerMovement = GetComponentInParent<PlayerMovementBehavior>();
     }
 
-    private void FixedUpdate()
+    private void OnTriggerEnter(Collider other)
     {
-        RaycastHit hitData;
-        Physics.Raycast(_detector, out hitData);
-
-        if (hitData.collider.tag == "snowball")
+        if (other.CompareTag("snowball"))
         {
-            Debug.Log("There was a hit, score should increase");
-            GameManagerScript.IncreaseScore();
+            if (!_playerMovement.IsGrounded && !_playerMovement.IsOnRope)
+                GameManagerScript.IncreaseScore();
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Vector3 rayDir  = -transform.up * 100;
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, rayDir);
     }
 }
