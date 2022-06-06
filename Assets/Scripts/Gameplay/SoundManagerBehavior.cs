51d389b7f9a8d9405bb2a86bfe09627566ee879c
Katelyn <katelynwest225@gmail.com>
2022-06-06 15:53:08 -0500

    Created a sound manager to be responsible for playing different sounds

---
+++ Assets/Scripts/Gameplay/SoundManagerBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerBehavior : MonoBehaviour
+{
+    private static AudioSource _source;
+    private static AudioClip _clipToPlay;
+    private static bool _playClip;
+
+    public static bool PlayClip { get => _playClip; set => _playClip = value; }
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (PlayClip)
+        {
+            _source.Play();
+            PlayClip = false;
+        }
+    }
+
+    static public void setSoundClip(AudioClip clip)
+    {
+        _clipToPlay = clip;
+        _source.clip = _clipToPlay;
+    }
+}
