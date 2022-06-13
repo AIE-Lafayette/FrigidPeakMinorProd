c1dd0fc5e86dc505f2d6009b070b4bd82a2e7c3a
Will <montero_w@live.com>
2022-06-13 15:57:40 -0500

    Started On Yeti Animation

---
+++ Assets/Scripts/AnimationBehaviors/YetiAnimationBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetiAnimationBehaviour : MonoBehaviour
+{
+
+    private Animator _animation;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        _animation = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _animation
+    }
+}
