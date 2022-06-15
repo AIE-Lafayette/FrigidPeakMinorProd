6c0229ad516b50c82c109a83c78e0ee346cc51c0
Katelyn <katelynwest225@gmail.com>
2022-06-15 15:40:57 -0500

    Made it so where if a player is grounded, IsClimbing is set to false

--- Assets/GroundColliderBehavior.cs
+++ Assets/GroundColliderBehavior.cs
@@ -10,7 +10,10 @@ public class GroundColliderBehavior : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ground"))
+        {
             _playermovement.IsGrounded = true;
+            _playermovement.IsClimbing = false;
+        }
     }
 
     private void OnTriggerExit(Collider other)
