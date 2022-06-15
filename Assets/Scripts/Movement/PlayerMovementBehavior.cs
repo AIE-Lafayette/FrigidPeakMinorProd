1861921aff72d8e5a30715b8555df3afb8cb9ad1
Katelyn <katelynwest225@gmail.com>
2022-06-15 11:31:26 -0500

    Finished player animations and changed pause menu color

--- Assets/Scripts/Movement/PlayerMovementBehavior.cs
+++ Assets/Scripts/Movement/PlayerMovementBehavior.cs
@@ -64,12 +64,11 @@ public class PlayerMovementBehavior : MonoBehaviour
         //if the player is grounded
         if (_isGrounded)
         {
-            //SoundManagerBehavior.setSoundClip(_jumpClip);
+            _isGrounded = false;
             //SoundManagerBehavior.PlayClip = true;
             _jumpSource.Play();
             // Add a force to push the player upward.
             _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _isGrounded = false;
         }
     }
 
